3fd15213300191c280bdaf3cc12221fa406ab202
Promsurin Phutthammawong <promsurinm@hotmail.com>
2022-03-05 21:41:35 +0700

    polo

--- PWTA/point_the_area.py
+++ PWTA/point_the_area.py
@@ -1,11 +1,13 @@
 r = float(input("Please enter R: "))
 amount = 0
 if r > 0:
+
+    #Loop through x and y inside R
     for x in range(1,int(r)+1):
         for y in range (1,int(r)+1):
             #print(x,y)
             if ((x**2+y**2)**0.5) <= r:
-                amount  += 1
+                amount += 1
     print("Point within the area =",amount)
 else:
     print("Invalid Input.")
