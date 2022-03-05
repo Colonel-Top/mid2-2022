bc5f3e60b3778254c7f2bd66fb0df90079e2d561
Promsurin Phutthammawong <promsurinm@hotmail.com>
2022-03-05 21:17:12 +0700

    Polo

--- cc_car.py
+++ cc_car.py
@@ -1,7 +1,14 @@
-cc = int("Enter cc:")
+cc = int(input("Enter cc:"))
 tax = 0
-if cc <=600:
-    tax ....................
+if cc <= 600:
+    tax = cc * 0.5
+elif cc > 600 and cc <= 1800:
+    cc_ext = cc - 600
+    cc_ext *= 1.5
+    tax = cc_ext + 300
+elif cc > 1800:
+    cc -= 1800
+    cc *= 4
+    tax = cc + 300 + 1800
 
-if ..................:
-
+print("Tax =",tax)
